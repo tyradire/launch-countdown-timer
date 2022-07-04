1a1521e1962f98fa7f82a052f493f0d7c4b53280
Daniel Ugarov <tyradire@yandex.ru>
2022-07-04 17:32:30 +0300

    fix index.js countdown logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,27 @@
-const hours = document.querySelector('.timer__hours');
-const minutes = document.querySelector('.timer__minutes');
-const seconds = document.querySelector('.timer__seconds');
+const days = document.querySelector('.timer__number-days');
+const hours = document.querySelector('.timer__number-hours');
+const minutes = document.querySelector('.timer__number-minutes');
+const seconds = document.querySelector('.timer__number-seconds');
 
-const time = {
-  d: 0,
-  h: 0,
-  m: 0,
-  s: 0
+const countdown = () => {
+  const countDate = new Date('September 13, 2022 00:00:00').getTime();
+  const nowDate = new Date().getTime();
+  const gap = countDate - nowDate;
+
+  const s = 1000;
+  const m = s * 60;
+  const h = m * 60;
+  const d = h * 24;
+
+  const textDay = Math.floor(gap / d);
+  const textHour = Math.floor((gap % d) /h);
+  const textMinute = Math.floor((gap % h) /m);
+  const textSecond = Math.floor((gap % m) /s);
+
+  days.textContent = textDay < 10 ? '0' + textDay : textDay;
+  hours.textContent = textHour < 10 ? '0' + textHour : textHour;
+  minutes.textContent = textMinute < 10 ? '0' + textMinute : textMinute;
+  seconds.textContent = textSecond < 10 ? '0' + textSecond : textSecond;
 }
 
-let timer;
-
-const init = () => {
-  timer = new Date();
-}
-
-init();
-
-console.log(timer)
+setInterval(countdown, 1000);
